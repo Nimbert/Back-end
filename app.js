98b2035085af1028df579d13464932864b982967
Nimbert <nimbert333@gmail.com>
2020-10-05 10:11:41 +0800

    my first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require ('express');
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const config = require("./config/database");
+
+//DATABASE
+mongoose.connect(config.database);
+
+mongoose.connection.on('connected', ()=> {
+    console.log('connected to database' +config.database); 
+}
+);
+
+mongoose.connection.on('error', (err)=> {
+    console.log('connection error' +config.database); 
+}
+);
+
+const app = express();
+const users = require('./routes/users');
+
+//port number
+const port = 3000;
+
+//CORS Middleware 
+app.use(cors());
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+//body parser middleware
+app.use(bodyParser.json());
+app.use('/users', users);
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport')(passport);
+//index route
+app.get('/', (req, res) => {res.send('invalid Endpoint')});
+
+//start server
+app.listen(port, () => {
+    console.log('server started on port' +port);
+});
